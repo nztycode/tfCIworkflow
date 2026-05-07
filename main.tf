73a1b23a7f19cac8028740c9527f58e790480ed1
NZTYCODE <NEZ7296@GMAIL.COM>
2026-05-07 18:56:14 +0800

    Adding new s3 bucket

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce12-tfstate-bucket"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket_prefix =   nas-s3buckets
+}
